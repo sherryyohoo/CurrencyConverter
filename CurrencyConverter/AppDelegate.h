92b2c3e7faa401a7734ac4d7ce3661216bc00108
sherryyohoo <scai@smith.edu>
2018-06-07 12:20:43 -0400

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Zhenyao Cai on 6/7/18.
+//  Copyright © 2018 Zhenyao Cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by Zhenyao Cai on 6/7/18.
+//  Copyright © 2018 Zhenyao Cai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
